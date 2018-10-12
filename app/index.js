248360b8b8c0d60d7152f3d4c81d611d555b56b5
Talia Grace <taliahaller@Talias-iMac.local>
2018-10-11 20:54:15 -0700

    button works

--- app/index.js
+++ app/index.js
@@ -105,38 +105,6 @@ if (!$('#menu').is('hidden')) {
 
 
 
-//Attempt at Carousel animation
-
-var
-    carousel = document.querySelector('.carousel'),
-    figure = carousel.querySelector('figure'),
-    nav = carousel.querySelector('.navigation'),
-    numImages = figure.childElementCount,
-    theta =  2 * Math.PI / numImages,
-    currImage = 0
-;
-
-nav.addEventListener('click', onClick, true);
-
-function onClick(e) {
-    e.stopPropagation();
-
-    var t = e.target;
-    if (t.tagName.toUpperCase() != 'BUTTON')
-        return;
-
-    if (t.classList.contains('next')) {
-        currImage++;
-    }
-    else {
-        currImage--;
-    }
-
-    figure.style.transform = `rotateY(${currImage * -theta}rad)`;
-}
-
-
-
 }); /*Document Ready Close*/
 
 
